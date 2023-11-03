d87765ae015f2f01584ad9be296a184b0aec0cea
Hewitt02M <masakurehewitt21@gmail.com>
2023-11-03 16:41:51 -0700

    execute.c

--- execute.c
+++ execute.c
@@ -8,7 +8,6 @@
  */
 void xcut_funct(const char *command)
 {
-<<<<<<< HEAD
 pid_t child_p = fork()
 }
 if (child_pid == -1)
@@ -25,22 +24,4 @@ exit(EXIT_FAILURE)
 else
 {
 wait(NULL);
-=======
-    pid_t child_pid = fork();
-    if (child_pid == -1)
-    {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-    else if (child_pid == 0)
-    {
-        execlp(command, command, (char *)NULL);
-        perror("execlp");
-        exit(EXIT_FAILURE);
-    }
-    else
-    {
-        wait(NULL);
-    }
->>>>>>> 5d2fe30546e9b74315c4bdc86ace79c1714c6399
 }

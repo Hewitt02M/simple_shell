e52df7f724ccee6f6afa8b31c551b3269af3eb75
Hewitt02M <masakurehewitt21@gmail.com>
2023-11-03 16:45:25 -0700

    prompt.c

--- prompt.c
+++ prompt.c
@@ -3,18 +3,9 @@
 /**
  * display_prompt - A function that displays the shell prompt.
  *
-<<<<<<< HEAD
- *  Return - nothing
- *
-*/
-
-void display_prompt(void) {
-jhew_print("jhew_shell$");
-=======
  * Return: No return value.
  */
 void display_prompt(void)
 {
     jhew_print("jhew_shell$ ");
->>>>>>> 5d2fe30546e9b74315c4bdc86ace79c1714c6399
 }
